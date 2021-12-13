1a2a5188a01c98f14b1feb2dfd6e3e30db0469fe
Mihaibbb <79415664+Mihaibbb@users.noreply.github.com>
2021-12-13 20:38:28 +0200

    Initialize chess-1.2-pawnTransform-&-localStorage-&-ui

---
+++ chess/src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, useRef } from "react";
+import Board from './Board';
+import RightSide from "./RightSide";
+import LeftSide from "./LeftSide";
+
+import '../styles/home.css';
+
+export default function Home() {
+    
+    return (
+        <div className="game">
+            <LeftSide />
+            <Board />
+            <RightSide />
+        </div>
+    );
+};
---
+++ chess/src/Components/RightSide.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ResetButton from './ResetButton';
+
+import '../styles/right-side.css';
+
+export default function RightSide() {
+
+    return (
+        <div className="right-side">
+            <ResetButton />
+        </div>
+    );
+};
